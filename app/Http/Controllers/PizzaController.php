9a61d4b69082a320d514e0c10de80756898cda99
WarWarKyaw <warwar67890@gmail.com>
2023-04-28 09:11:19 +0630

    Getting a Single Record

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -9,8 +9,8 @@ class PizzaController extends Controller
     public function index(){
     //    $pizzas=Pizza::all();
     // $pizzas=pizza ::OrderBy('name')->get();
-    $pizzas=pizza::where('type','haiiwan')->get();
-
+    // $pizzas=pizza::where('type','haiiwan')->get();
+    $pizzas=Pizza::latest()->get();
         
        
         return view('pizzas.index', [
@@ -21,7 +21,8 @@ class PizzaController extends Controller
     }
 
     public function show($id){
-        return view('pizzas.show', ['id'=>$id]);
+        $pizza=Pizza::findOrFail($id);
+        return view('pizzas.show', ['pizza'=>$pizza]);
 
     }
 

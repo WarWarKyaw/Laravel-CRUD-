840f3ea0f67ddaabd0f3b0ab9da670244c0874fd
WarWarKyaw <warwar67890@gmail.com>
2023-04-28 10:59:11 +0630

    Saving Records

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -31,10 +31,14 @@ class PizzaController extends Controller
     }
     
     public function store(){
-        error_log(request('name'));
-        error_log(request('type'));
-        error_log(request('base'));
+        $pizza=new Pizza();
+        $pizza->name=request('name');
+        $pizza->type=request('type');
+        $pizza->base=request('base');
+         $pizza->save();
+        
 
-    return redirect('/');
+
+    return redirect('/')->with('msg', 'Thank for your Order');
     }
 }

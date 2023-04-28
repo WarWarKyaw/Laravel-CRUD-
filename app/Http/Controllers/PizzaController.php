1d7c9796d454c14abe508ceaaf1628968bc265c1
WarWarKyaw <warwar67890@gmail.com>
2023-04-28 10:29:05 +0630

    Post Requests

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -9,8 +9,8 @@ class PizzaController extends Controller
     public function index(){
     //    $pizzas=Pizza::all();
     // $pizzas=pizza ::OrderBy('name')->get();
-    $pizzas=pizza::where('type','haiiwan')->get();
-
+    // $pizzas=pizza::where('type','haiiwan')->get();
+    $pizzas=Pizza::latest()->get();
         
        
         return view('pizzas.index', [
@@ -21,11 +21,20 @@ class PizzaController extends Controller
     }
 
     public function show($id){
-        return view('pizzas.show', ['id'=>$id]);
+        $pizza=Pizza::findOrFail($id);
+        return view('pizzas.show', ['pizza'=>$pizza]);
 
     }
 
     public function create(){
         return view('pizzas.create');
+    }
+    
+    public function store(){
+        error_log(request('name'));
+        error_log(request('type'));
+        error_log(request('base'));
+
+    return redirect('/');
     }
 }
